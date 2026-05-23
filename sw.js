d992ae172522b5e0313dbe802981e3f6e90a348c
chuyiouart <109233749+chuyiouart@users.noreply.github.com>
2026-05-23 09:58:59 +0800

    Add licensing and revenue model page

---
+++ sw.js
@@ -0,0 +1,51 @@
+const METRION_CACHE = "metrion-pwa-v3";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./services.html",
+  "./licensing.html",
+  "./styles.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./assets/logo.jpg",
+  "./assets/logo-transparent.png",
+  "./assets/apple-touch-icon.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+  "./assets/hero-display.jpg",
+  "./assets/display-close.jpg",
+  "./assets/sample-cards.jpg",
+  "./assets/application-card.jpg",
+  "./cases/test-series.html",
+  "./cases/foreign-artist.html",
+  "./cases/youth-creator.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(METRION_CACHE).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== METRION_CACHE).map((key) => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(METRION_CACHE).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
